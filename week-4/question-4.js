8d96feafa238e38a2bf1ac429e6fe35edf52cd99
Ehsanullah946 <ehsanullahakbari53@gmail.com>
2025-12-09 14:07:45 +0430

    changed some function s

--- week-4/question-4.js
+++ week-4/question-4.js
@@ -1,17 +1,20 @@
 // Author: Ehsanullah Akbari
 // Date: 2025-11-30
-// exercise 4 of week-4  Description: Return elements present in both arrays. 
-// Example: 
-// Input: [1, 2, 3, 4], [3, 4, 5, 6] 
-// Output: [3, 4] 
+// exercise 4 of week-4  Description: Return elements present in both arrays.
+// Example:
+// Input: [1, 2, 3, 4], [3, 4, 5, 6]
+// Output: [3, 4]
+
+
 export const intersectionArray = (array1, array2) => {
-    let result = [];
+    let result ={};
       for (let index = 0; index < array1.length; index++) {
-          if (array1.includes(array2[index])) {
-              result.push(array2[index])
-          } 
+          let element = array1[index];
+          if (array2.includes(element)) {
+              result[element] = element;
+          }     
     }
     return result;
 }
 
-console.log(intersectionArray([1, 2, 3, 4], [3, 4, 5, 6]));
+console.log(intersectionArray([1, 2, 3, 4], [3, 4, 4, 6]));
