da65dd284f174dd8ef0e08f444aca1b22a21001f
Ehsanullah946 <ehsanullahakbari53@gmail.com>
2025-12-09 14:37:38 +0430

    new stack

---
+++ week-5/question-4.js
@@ -0,0 +1,46 @@
+
+
+class MinStack {
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    push(item) {
+      this.stack[this.stack.length] = item;      
+        if (this.minStack.length === 0 || item <= this.getMin()) {
+             this.minStack[this.minStack.length]= item;
+        }                           
+    }
+    pop() {
+        return this.stack[this.stack.length--];
+    }
+    getMin() {
+       return  this.minStack[this.minStack.length-1];
+    }
+
+    printMinStack() {
+        let newArray = [];
+       for (let i = 0; i < this.stack.length; i++) {
+          newArray.push(this.stack[i])
+        }
+        return newArray;
+   }
+
+}
+
+
+const stack = new MinStack();
+
+stack.push(20);
+
+stack.push(10);
+(stack.pop());
+stack.push(11);
+stack.push(5);
+stack.push(50);
+console.log(stack.printMinStack());
+console.log(stack.getMin());
+
+
+
